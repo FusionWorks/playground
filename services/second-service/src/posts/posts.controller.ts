db4df496a49fe0beaf08a5f2a7f1ffeddcbb3f2b
VladMorari1 <vmorari@fusionworks.md>
2022-12-27 11:52:27 +0200

    Added TransformResponse interceptor

--- services/second-service/src/posts/posts.controller.ts
+++ services/second-service/src/posts/posts.controller.ts
@@ -1,6 +1,5 @@
 import PostsService from './posts.service';
-import { plainToClass } from '@nestjs/class-transformer';
-import { PostsDto } from './dto/posts.dto';
+
 import {
   Body,
   Controller,
@@ -10,43 +9,36 @@ import {
   Patch,
   Post,
   Put,
+  UseInterceptors,
 } from '@nestjs/common';
 import CreatePostsDto from './dto/create-posts.dto';
 import ParamsWithId from './utils/paramsWithId';
 import UpdatePostDto from './dto/update-post.dto';
+import { ResponseTransformInterceptor } from './posts.interceptor';
 
 @Controller()
+@UseInterceptors(ResponseTransformInterceptor)
 export default class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
   async getAllPosts() {
-    const allPosts = await this.postsService.findAll();
-
-    return plainToClass(PostsDto, allPosts, { excludeExtraneousValues: true });
+    return this.postsService.findAll();
   }
 
   @Get(':id')
   async getPostById(@Param() { id }: ParamsWithId) {
-    const postById = await this.postsService.findOne(id);
-
-    return plainToClass(PostsDto, postById, { excludeExtraneousValues: true });
+    return this.postsService.findOne(id);
   }
 
   @Post()
   async createPost(@Body() post: CreatePostsDto) {
-    const newPost = await this.postsService.create(post);
-
-    return plainToClass(PostsDto, newPost, { excludeExtraneousValues: true });
+    return this.postsService.create(post);
   }
 
   @Delete(':id')
   async deletePost(@Param() { id }: ParamsWithId) {
-    const deletedPost = await this.postsService.delete(id);
-
-    return plainToClass(PostsDto, deletedPost, {
-      excludeExtraneousValues: true,
-    });
+    return this.postsService.delete(id);
   }
 
   @Put(':id')
@@ -54,11 +46,7 @@ export default class PostsController {
     @Param() { id }: ParamsWithId,
     @Body() post: CreatePostsDto,
   ) {
-    const updatedPost = await this.postsService.update(id, post);
-
-    return plainToClass(PostsDto, updatedPost, {
-      excludeExtraneousValues: true,
-    });
+    return this.postsService.update(id, post);
   }
 
   @Patch(':id')
@@ -66,10 +54,6 @@ export default class PostsController {
     @Param() { id }: ParamsWithId,
     @Body() post: UpdatePostDto,
   ) {
-    const updatedPost = await this.postsService.partialUpdate(id, post);
-
-    return plainToClass(PostsDto, updatedPost, {
-      excludeExtraneousValues: true,
-    });
+    return this.postsService.partialUpdate(id, post);
   }
 }
